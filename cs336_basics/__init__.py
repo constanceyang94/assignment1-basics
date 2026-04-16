a7c1056fa5802be30cac25041edc900da0ee34c1
Constance <constanceyang94@gmail.com>
2026-04-15 22:59:15 -0700

    Train bpe tokenizer version with all test passed. Multi-processing and run time optimization are not applied yet.

--- cs336_basics/__init__.py
+++ cs336_basics/__init__.py
@@ -4,26 +4,3 @@ try:
     __version__ = importlib.metadata.version("cs336_basics")
 except importlib.metadata.PackageNotFoundError:
     pass
-
-
-from collections import defaultdict
-
-# 初始化一个默认值为 0 的字典
-byte_counts = defaultdict(int)
-
-# 模拟读取 UTF-8 编码的字节流
-text_bytes = "hello".encode('utf-8') # b'\xe4\xbd\xa0\xe5\xa5\xbd'
-
-for b in text_bytes:
-    print(b)
-    byte_counts[b] += 1
-
-print(text_bytes)
-print(type(text_bytes))
-print(dict(byte_counts)) 
-cur_pair = text_bytes[0:2]
-print(dict(byte_counts)) 
-for i in range(len(text_bytes) - 1):
-    pair = text_bytes[i:i+2]
-    
-# 输出每个字节及其出现的频率
--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -1,6 +1,10 @@
 import regex as re
 import os
+
 # from .pretokenization_example import find_chunk_boundaries
+
+PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+
 
 class BPETokenizer:
     def __init__(self, vocab, merges, special_tokens):
@@ -44,39 +48,96 @@ def train_bpe_tokenizer(
                 representing that <token1> was merged with <token2>.
                 Merges are ordered by order of creation.
     """
-    # Plan: 
+    # Plan:
     # 1. Pre-tokenizer
     # 2. Compute BPE merges
     # 3. Change pre-tokenizer to multiprocessing using pre-tokenization example
-    pre_tokenizer_counts = {} # Dictionary mapping pre-token (bytes) to count (int)
-    merges = [] # List of merges, each merge is a tuple of bytes (<token1>, <token2>)
-    vocab = {i: bytes([i]) for i in range(256)} # Initialize vocab with single byte tokens
-    pair_counts = {} # Dictionary mapping pairs of tokens (tuple of bytes) to count (int)
-    special_pattern = "|".join(re.escape(t) for t in sorted(special_tokens, key=len, reverse=True))
-    with open(input_path, "rb") as f:
-        chunk = f.read().decode("utf-8", errors="ignore")
-        for match in re.finditer(special_pattern, chunk):
-            token = match.group()
-            pre_tokenizer_counts[token] = pre_tokenizer_counts.get(token, 0) + 1
-    for token in pre_tokenizer_counts.keys():
-        for pair in zip(token, token[1:]):
-            pair_counts[pair] = pair_counts.get(pair, 0) + 1
-    best_pair = max(pair_counts, key=pair_counts.get)
-    merges.append(best_pair)
-    vocab[len(vocab)] = bytes(best_pair)
+    pre_tokenizer_counts = {}  # Dictionary mapping pre-token (bytes) to count (int)
+    merges = []  # List of merges, each merge is a tuple of bytes (<token1>, <token2>)
+    vocab = {
+        i: bytes([i]) for i in range(256)
+    }  # Initialize vocab with single byte tokens
+    if vocab_size - len(special_tokens) < 256:
+        return vocab, merges
+    for special_token in special_tokens:
+        vocab[len(vocab)] = bytes(special_token.encode("utf-8"))
+    pre_tokenizer_counts = pretokenize(input_path, special_tokens)
     for _ in range(vocab_size - len(special_tokens) - 256):
-        pair_counts = {}
-        for token in pre_tokenizer_counts.keys():
-            new_token = []
-            for i in range(len(token) - 1):
-                pair = (token[i], token[i+1])
-                if pair == best_pair:
-                    new_token.append(bytes(pair))
-                    i += 2
-                else:
-                    new_token.append(token[i])
-        best_pair = max(pair_counts, key=pair_counts.get)
-        merges.append(best_pair)
+        compute_single_bpe_merge(pre_tokenizer_counts, merges, vocab)
     return vocab, merges
 
-    
+
+def pretokenize(input_path: str | os.PathLike, special_tokens: list[str]):
+    """
+    Given the input path to BPE tokenizer training data and special tokens, splits around special
+    tokens, and applies the GPT-2 regex pattern to count pre-token occurrences.
+
+    Args:
+        input_path: input path to training data.
+        special_tokens: to split input data.
+    """
+    pre_tokenizer_counts = {}
+    special_pattern = "|".join(
+        re.escape(t) for t in sorted(special_tokens, key=len, reverse=True)
+    )
+    last_end = 0
+    with open(input_path, "rb") as f:
+        chunk = f.read().decode("utf-8", errors="ignore")
+        if len(special_tokens) >= 1:
+            for match in re.finditer(special_pattern, chunk):
+                paragraph = chunk[last_end : match.start()]
+                last_end = match.end()
+                pretokenize_helper(pre_tokenizer_counts, paragraph)
+        paragraph = chunk[last_end : len(chunk)]
+        pretokenize_helper(pre_tokenizer_counts, paragraph)
+    return pre_tokenizer_counts
+
+
+def pretokenize_helper(
+    pre_tokenizer_counts: dict[tuple[bytes, ...], int], paragraph: str
+):
+    """
+    pretokenize_helper which takes paragraph as input and mutate the pre_tokenizer_counts in place.
+    """
+    for token_match in re.finditer(PAT, paragraph):
+        token = token_match.group().encode("utf-8")
+        token_tuple = tuple(bytes([b]) for b in token)
+        pre_tokenizer_counts[token_tuple] = pre_tokenizer_counts.get(token_tuple, 0) + 1
+
+
+def compute_single_bpe_merge(
+    pre_tokenizer_counts: dict[tuple[bytes, ...], int],
+    merges: list[tuple[bytes, bytes]],
+    vocab: dict[int, bytes],
+):
+    """
+    Single BPE merge during tokenizer. Merge the token pair with the highest frequency and mutate
+    pre_tokenizer_counts in place after the merge.
+    The function modifies merge and vocab in place as well.
+    """
+    pair_counts = {}
+    for token in pre_tokenizer_counts.keys():
+        for i in range(len(token) - 1):
+            pair = (token[i], token[i + 1])
+            pair_counts[pair] = pair_counts.get(pair, 0) + pre_tokenizer_counts[token]
+    best_pair = max(pair_counts, key=lambda p: (pair_counts[p], p))
+    merges.append(best_pair)
+    vocab[len(vocab)] = best_pair[0] + best_pair[1]
+    for token in list(pre_tokenizer_counts.keys()):
+        token_tuple_new = []
+        i = 0
+        while i < len(token):
+            if i == len(token) - 1:
+                token_tuple_new.append(token[i])
+                break
+            pair = (token[i], token[i + 1])
+            if pair == best_pair:
+                token_tuple_new.append(pair[0] + pair[1])
+                i += 2
+            else:
+                token_tuple_new.append(token[i])
+                i += 1
+        token_tuple_new = tuple(token_tuple_new)
+        if token_tuple_new != token:
+            pre_tokenizer_counts[token_tuple_new] = pre_tokenizer_counts[token]
+            del pre_tokenizer_counts[token]
