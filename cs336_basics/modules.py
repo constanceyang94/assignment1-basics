c23ec343564b1ba361d1e7e3effc929128934916
Constance <constanceyang94@gmail.com>
2026-08-15 21:42:56 -0700

    Added RMSNorm

--- cs336_basics/modules.py
+++ cs336_basics/modules.py
@@ -47,3 +47,33 @@ class Embedding(torch.nn.Module):
         
     def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
         return self.weights[token_ids]
+    
+
+class RMSNorm(torch.nn.Module):
+    "Root Mean Square Layer Normalization"
+    def __init__(self, d_model: int, eps: float = 1e-5, device=None, dtype=None):
+        """
+        d_model: int  Hidden dimension of the model
+        eps: float = 1e-5  Epsilon value for numerical stability
+        device: torch.device | None = None  Device to store the parameters on
+        dtype: torch.dtype | None = None  Data type of the parameters
+        """
+        super().__init__()
+        self.d_model = d_model
+        self.eps = eps
+        if device:
+            self.device = device
+        if dtype:
+            self.dtype = dtype 
+        self.weights = torch.nn.Parameter(torch.ones(self.d_model))
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Process an input tensor of shape
+        (batch_size, sequence_length, d_model) and return a tensor of the same shape
+        """
+        in_dtype = x.dtype
+        x = x.to(torch.float32)
+        rms = torch.sqrt(torch.mean(x**2, dim=-1, keepdim=True) + self.eps)
+        result = x / rms * self.weights
+        return result.to(in_dtype)
